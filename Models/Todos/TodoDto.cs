8d96f3d0db8dd3e042e434117da39e6b4217d05c
Joey Mckenzie <JMckenzie@spi-ind.com>
2020-06-23 11:50:53 -0700

    feat(Project): Initial commit

---
+++ Models/Todos/TodoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace StateManagementWithFluxor.Models.Todos
+{
+    public class TodoDto
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Body { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ Store/State/TodosState.cs
@@ -0,0 +1,15 @@
+﻿using StateManagementWithFluxor.Models.Todos;
+using System.Collections.Generic;
+
+namespace StateManagementWithFluxor.Store.State
+{
+    public class TodosState
+    {
+        public TodosState(bool isLoading, IEnumerable<TodoDto>? currentTodos) =>
+            (IsLoading, CurrentTodos) = (isLoading, currentTodos);
+
+        public bool IsLoading { get; }
+
+        public IEnumerable<TodoDto>? CurrentTodos { get; }
+    }
+}
